2816512383683b8b850db900453ababe006f8194
David Mallon <mallond@gmail.com>
2026-02-10 14:05:22 -0800

    save point for docker-local-gpu

---
+++ squid/docker-local-gpu/openclaw/ecosystem.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+{
+  name: "keepalive",
+  script: "/data/dummy.js",   // create this tiny file next to ecosystem.config.js
+  exec_mode: "fork",
+  autorestart: true
+},
+    {
+      name: "openclaw-gateway",
+      script: "/usr/local/bin/openclaw",
+      args: "gateway --bind lan --port 18789 --verbose",
+      exec_mode: "fork",
+      autorestart: false,
+      max_restarts: 10,
+      restart_delay: 1000,
+      env: {
+        OPENCLAW_STATE_DIR: "/data",
+        OPENCLAW_CONFIG_PATH: "/data/openclaw.json",
+        HOME: "/data"
+      }
+    }
+    
+  ]
+};
+
